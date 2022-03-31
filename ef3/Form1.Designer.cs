7c773cf13e16d111bbed50520651c548e39185ea
Tarik Bagriyanik <tbagriyanik@gmail.com>
2022-03-31 15:52:45 +0300

    ilk

---
+++ ef3/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace ef3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SunkenHorizontal;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 14);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(338, 423);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.Location = new System.Drawing.Point(492, 73);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(182, 23);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(412, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 17);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Sayı";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(492, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(182, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(412, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 17);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Metin";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.ForeColor = System.Drawing.Color.Blue;
+            this.button3.Location = new System.Drawing.Point(492, 215);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(182, 48);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "💖 Güncelle";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(492, 167);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(182, 48);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "💔 Sil";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.DarkGreen;
+            this.button1.Location = new System.Drawing.Point(492, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(182, 48);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "🤍 Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(411, 373);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 19);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Hoşgeldiniz...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(743, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Entity Framework";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ef3/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ef3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //https://www.yazilimkodlama.com/programlama/c-windows-form-entity-framework-veritabani-baglantisi/
+
+        vtEntities _db = new vtEntities();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            doldur();
+        }
+
+        private void doldur()
+        {
+            _db.SaveChanges();
+            dataGridView1.DataSource = _db.tabloes.ToList();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int rowIndex = e.RowIndex;
+            if (rowIndex != -1)
+            {
+                DataGridViewRow row = dataGridView1.Rows[rowIndex];
+                textBox1.Text = row.Cells[1].Value.ToString();
+                textBox2.Text = row.Cells[2].Value.ToString();
+                label3.Text = "Kayıt seçildi " + DateTime.Now.ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //ekle
+            if (!String.IsNullOrEmpty(textBox1.Text))
+            {
+                tablo _tablo = new tablo();
+                _tablo.Ad = textBox1.Text;
+                _tablo.Ucret = Convert.ToDouble(textBox2.Text);
+                _db.tabloes.Add(_tablo);
+                doldur();
+                textBox1.Text = "";
+                textBox2.Text = "0";
+                label3.Text = "Kayıt eklendi. " + DateTime.Now.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Metin kutusu boş...");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //sil            
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                if (MessageBox.Show("Kayıt silme içi emin misiniz?", "Silme Onayı", MessageBoxButtons.YesNo, MessageBoxIcon.Stop, MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+                {
+                    for (int i = 0; i < dataGridView1.SelectedRows.Count; i++)
+                    {
+                        DataGridViewRow row = dataGridView1.SelectedRows[i];
+                        int silinecekID = (int)row.Cells[0].Value;
+                        var silinecek = _db.tabloes.Where(w => w.id == silinecekID).FirstOrDefault();
+                        _db.tabloes.Remove(silinecek);
+                        label3.Text = "Kayıt silindi. " + DateTime.Now.ToString();
+                    }
+                    doldur();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Seçili bir kayıt yok...");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //güncelle
+            int nerede = dataGridView1.CurrentCell.RowIndex;
+            if (dataGridView1.CurrentCell.RowIndex > -1 && !String.IsNullOrEmpty(textBox1.Text))
+            {
+                int rowIndex = dataGridView1.CurrentCell.RowIndex;
+                DataGridViewRow row = dataGridView1.Rows[rowIndex];
+                int guncellenecekID = (int)row.Cells[0].Value;
+                var guncellenecek = _db.tabloes.Where(w => w.id == guncellenecekID).FirstOrDefault();
+                guncellenecek.Ad = textBox1.Text;
+                guncellenecek.Ucret = Convert.ToDouble(textBox2.Text);
+                textBox1.Text = "";
+                textBox2.Text = "0";
+                doldur();
+
+                //imleci eski yerine alalım
+                dataGridView1.CurrentCell = dataGridView1.Rows[nerede].Cells[0];
+
+                label3.Text = "Kayıt güncellendi. " + DateTime.Now.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Seçili bir kayıt yok veya boş giriş ...");
+            }
+        }
+    }
+}
